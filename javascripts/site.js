bb6d42802f51585d9a27cb84da2d03d0f166efa9
Daniel Park <d.hanul.park@gmail.com>
2017-11-29 22:18:14 -0500

    Automated commit at 2017-11-30 03:18:14 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,50 +1,50 @@
-// This is where it all goes :)
+// // This is where it all goes :)
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
 
-  $('.percentage').easyPieChart({
-    animate: 1000,
-    lineWidth: 9,
-    scaleColor: false, 
-    barColor: "#ffffff",
-    trackColor: "#000000",
-    size: 300
-  });
+//   $('.percentage').easyPieChart({
+//     animate: 1000,
+//     lineWidth: 9,
+//     scaleColor: false, 
+//     barColor: "#ffffff",
+//     trackColor: "#000000",
+//     size: 300
+//   });
 
 
-   $('.reveal').hover(function() {
-      if ($(this).data("reality") === "reality") {
-        $('.bounce').toggleClass('hide');
-        $('.hover_bounce').toggleClass('show');
-      }
+//    $('.reveal').hover(function() {
+//       if ($(this).data("reality") === "reality") {
+//         $('.bounce').toggleClass('hide');
+//         $('.hover_bounce').toggleClass('show');
+//       }
 
-      if ($(this).data("reality") === "augmented") {
-        $('.opacity').toggleClass('hide');
-        $('.hover_opacity').toggleClass('show');
-      }
+//       if ($(this).data("reality") === "augmented") {
+//         $('.opacity').toggleClass('hide');
+//         $('.hover_opacity').toggleClass('show');
+//       }
 
-      if ($(this).data("reality") === "mixed") {
-        $('.rotate').toggleClass('hide');
-        $('.hover_rotate').toggleClass('show');
-      }
+//       if ($(this).data("reality") === "mixed") {
+//         $('.rotate').toggleClass('hide');
+//         $('.hover_rotate').toggleClass('show');
+//       }
 
-      if ($(this).data("reality") === "virtual") {
-        $('.rotate_long').toggleClass('hide');
-        $('.hover_rotate_long').toggleClass('show');
-      }
-    });
-  /* Every time the window is scrolled ... */
-  $(window).scroll( function(){
-    /* Check the location of each desired element */
-    $('.block').each( function(i){
-      var bottom_of_object = $(this).position().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-      /* If the object is completely visible in the window, fade it it */
-      if( bottom_of_window + 800 > bottom_of_object ){
-        $(this).animate({'opacity':'1'},1000);       
-      }
-    }); 
-  }); 
-});
+//       if ($(this).data("reality") === "virtual") {
+//         $('.rotate_long').toggleClass('hide');
+//         $('.hover_rotate_long').toggleClass('show');
+//       }
+//     });
+//   /* Every time the window is scrolled ... */
+//   $(window).scroll( function(){
+//     /* Check the location of each desired element */
+//     $('.block').each( function(i){
+//       var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//       var bottom_of_window = $(window).scrollTop() + $(window).height();
+//       /* If the object is completely visible in the window, fade it it */
+//       if( bottom_of_window + 800 > bottom_of_object ){
+//         $(this).animate({'opacity':'1'},1000);       
+//       }
+//     }); 
+//   }); 
+// });
